1ccd52a28afa862339f07c4d30b0e305bbe810e5
Jill-Jênn Vie <jill-jenn.vie@riken.jp>
2018-09-17 10:13:45 +0900

    Fix creation of sparse tensor

--- tf_data.py
+++ tf_data.py
@@ -1,4 +1,4 @@
-from scipy.sparse import coo_matrix, csr_matrix
+from scipy.sparse import coo_matrix, csr_matrix, load_npz, find
 import tensorflow as tf
 import numpy as np
 import time
@@ -22,11 +22,24 @@ import time
 
 # x_batch, y_batch = sess.run(next_element)
 
-M = np.array([[0, 1, 1], [1, 0, 0], [1, 0, 0.5], [0, 1, 1]])
-print(M)
+# M = np.array([[0, 1, 1], [1, 0, 0], [1, 0, 0.5], [0, 1, 1]])
+# print(M)
 
-S = coo_matrix(M)
-S_tf_val = tf.SparseTensor(indices=np.column_stack((S.row, S.col)), values=S.data, dense_shape=M.shape)
+# S = coo_matrix(M)
+X_fm = load_npz('data/movie100k/X_fm.npz')
+S = X_fm[[1, 2], :].tocoo()
+print(S.row, S.col, S.data, S.shape)
+entries = np.column_stack((S.row, S.col, S.data))
+indices = np.lexsort((S.col, S.row))
+print(entries[indices, 0], entries[indices, 1], entries[indices, 2])
+S = X_fm[[1, 2], :].tocoo()
+# rows, cols, data = find(S)
+# print(rows, cols, data)
+# S = X_fm[1:3, :].tocoo()
+print(S.row, S.col, S.data, S.shape)
+# S_tf_val = tf.SparseTensor(indices=np.column_stack((S.row, S.col)), values=S.data, dense_shape=S.shape)
+S_tf_val = tf.SparseTensor(indices=entries[indices, :2], values=entries[indices, 2], dense_shape=S.shape)
+# S_tf_val = tf.SparseTensor(indices=np.column_stack((rows, cols)), values=data, dense_shape=S.shape)
 
 y_train = np.random.random(len(S.data))
 batch_size = 2
@@ -43,6 +56,7 @@ with tf.Session() as sess:
     while True:
         try:
             print(sess.run([X_fm_batch, outcomes]))
+            break
         except tf.errors.OutOfRangeError:
             print('Finished')
             break
