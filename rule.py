ee2dd0131f1679848f6f35b71a71bb43187228c4
Jill-Jênn Vie <vie@jill-jenn.net>
2022-11-20 00:38:19 +0900

    Refactor curves

--- rule.py
+++ rule.py
@@ -30,42 +30,15 @@ fig_name = log_name.replace('txt', 'pdf')
 fig_name_png = log_name.replace('txt', 'png')
 print(log_name)
 cv = 'trainval' not in log_name and 'ongoing_test' not in log_name
+cv = False
 
 LIBFM_RESULTS_PATH = Path('../Scalable-Variational-Bayesian-Factorization-Machine/results/')
+MAX_EPOCH = 200 # max(data['metrics']['test']['epoch'])
 
 criteria = defaultdict(lambda: defaultdict(list))
 with open(log_name) as f:
     data = json.load(f)
 embedding_size = data['args']['d']
-dataset = data['args']['data']
-
-als = {
-}
-
-vbfm = {
-    'movie100k': LIBFM_RESULTS_PATH / f'vb_{dataset}_{embedding_size}.csv',
-    'movie100k-binary': LIBFM_RESULTS_PATH / 'vbfm_100k_binary.csv',
-}
-
-ovbfm = {
-    'movie100k': LIBFM_RESULTS_PATH / f'vb_online_{dataset}_{embedding_size}.csv',
-    'movie1M': LIBFM_RESULTS_PATH / 'ovbfm_1M',
-    'movie10M': LIBFM_RESULTS_PATH / 'ovbfm_10M'
-}
-
-mcmc = {
-    'movie100': LIBFM_RESULTS_PATH / 'mcmc_100',
-    'movie1000': LIBFM_RESULTS_PATH / 'mcmc_1000',
-    'movie100k': LIBFM_RESULTS_PATH / f'mcmc_{dataset}_{embedding_size}.csv',
-    'movie100k-binary': LIBFM_RESULTS_PATH / 'mcmc_100k_binary.csv',
-    'movie1M': LIBFM_RESULTS_PATH / 'mcmc_1M',
-    'movie1M-binary': LIBFM_RESULTS_PATH / 'mcmc_1M_binary.csv',
-    'movie10M': LIBFM_RESULTS_PATH / 'mcmc_10M'
-}
-
-for dic in [vbfm, ovbfm, mcmc]:
-    print(dic['movie100k'])
-
 dataset = data['args']['data']
 metric_name = 'acc' if dataset in {
     'fraction', 'movie5', 'movie20', 'movie100',
@@ -73,6 +46,11 @@ metric_name = 'acc' if dataset in {
 CPP_METRIC = {
     'last': f'{metric_name}_mcmc_this',
     'mean': f'{metric_name}_mcmc_all'
+}
+METHODS = {
+    'vb': ('VBFM', ['last']),
+    'vb_online': ('OVBFM', ['last']),
+    'mcmc': ('MCMC', ['last', 'mean'])
 }
 
 train_epochs = np.unique(sorted(data['metrics']['train']['epoch']))
@@ -125,14 +103,15 @@ else:
 if metric_name in data['metrics']['train']:
     metric.plot(train_epochs, data['metrics']['train'][metric_name], label='train {:s}'.format(metric_name))
 metric.plot(test_epochs, data['metrics']['test'][metric_name], **get_style_and_label('VFM last'))
-if 'rmse_all' in data['metrics']['test']:
-    metric.plot(test_epochs, data['metrics']['test']['rmse_all'], **get_style_and_label('VFM mean'))
+if 'rmse_all_of_mean' in data['metrics']['test']:
+    metric.plot(test_epochs, data['metrics']['test']['rmse_all_of_mean'], **get_style_and_label('VFM mean'))
+    print('VFM mean', data['metrics']['test']['rmse_all_of_mean'][:MAX_EPOCH][-1])
 elif 'acc_all' in data['metrics']['test']:
     metric.plot(test_epochs, data['metrics']['test']['acc_all'], **get_style_and_label('VFM mean'))
+    print('VFM mean ACC', data['metrics']['test']['acc_all'][:MAX_EPOCH][-1])
+    print('VFM mean AUC', data['metrics']['test']['auc_all'][:MAX_EPOCH][-1])
+    print('VFM mean MAP', data['metrics']['test']['map_all'][:MAX_EPOCH][-1])
 
-# print('VFM', data['metrics']['test'][metric_name])
-MAX_EPOCH = 400 # max(data['metrics']['test']['epoch'])
-# 200 # 
 
 if cv:
     elbo.plot(train_epochs, data['metrics']['train']['elbo'], label='train elbo')
@@ -149,30 +128,19 @@ if cv:
     criterion_graph.legend()
     progress_graph.set_title('Progress of ELBO')
 
-if dataset in mcmc:
-    df = pd.read_csv(mcmc[dataset], sep='\t')
-    print(df.columns)
-    for displayed, mcmc_metric_name in CPP_METRIC.items():
-        metric.plot(1 + df.index[:MAX_EPOCH],
-            df[mcmc_metric_name][:MAX_EPOCH],
-            **get_style_and_label(f'MCMC {displayed}'))
-    # print('MCMC', df[mcmc_metric_name][:MAX_EPOCH])
 
-if dataset in vbfm:
-    df = pd.read_csv(vbfm[dataset], sep='\t')
-    print('vbfm', vbfm[dataset])
-    print(CPP_METRIC['last'])
-    print(df[CPP_METRIC['last']])
-    metric.plot(1 + df.index[:MAX_EPOCH], df[CPP_METRIC['last']][:MAX_EPOCH], **get_style_and_label('VBFM last'))
+for method, (displayed_method, metrics) in METHODS.items():
+    log_path = LIBFM_RESULTS_PATH / f'{method}_{dataset}_{embedding_size}.csv'
+    if log_path.is_file():
+        df = pd.read_csv(log_path, sep='\t')
+        if len(df) == 0:
+            continue
+        for eval_type in metrics:
+            metric.plot(1 + df.index[:MAX_EPOCH],
+                df[CPP_METRIC[eval_type]][:MAX_EPOCH],
+                **get_style_and_label(f'{displayed_method} {eval_type}'))
+            print(f'{displayed_method} {eval_type}', df[CPP_METRIC[eval_type]][:MAX_EPOCH].tail(1))
 
-if dataset in ovbfm:
-    df = pd.read_csv(ovbfm[dataset], sep='\t')
-    metric.plot(1 + df.index[:MAX_EPOCH], df[CPP_METRIC['last']][:MAX_EPOCH], **get_style_and_label('OVBFM last'))
-    # print('OVBFM', df['rmse_mcmc_all'][:MAX_EPOCH])
-
-if dataset in als:
-    df = pd.read_csv(als[dataset])
-    metric.plot(1 + df.index[:MAX_EPOCH], df['rmse'][:MAX_EPOCH], **get_style_and_label('libFM ALS'))
 
 metric.legend()
 if metric_name == 'rmse':
